c486d78b13d50b95ffa3b27289c428bdf8900a16
Corey <iJabba@outlook.com>
2020-04-18 21:43:10 +0100

    Updates

---
+++ precache-manifest.83cc1903880258ac62964f7963d58d8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28e9cf64dc37048d295617c74be1919",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "2928baaf08f00551a1ea",
+    "url": "/Covid-19-Tracker/static/css/main.0e65c819.chunk.css"
+  },
+  {
+    "revision": "56042001a7459708ea87",
+    "url": "/Covid-19-Tracker/static/js/2.4fa08a3b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-Tracker/static/js/2.4fa08a3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2928baaf08f00551a1ea",
+    "url": "/Covid-19-Tracker/static/js/main.accf923d.chunk.js"
+  },
+  {
+    "revision": "eebaca2ca868991201f4",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.9f3d5c86.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
