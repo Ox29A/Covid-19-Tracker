2f0a39a7004e55ef3b81a7049e9da56a1d518c88
Corey <iJabba@outlook.com>
2020-04-18 22:08:20 +0100

    Covid-19-Tracker: Added a navigation bar and material ui icons.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
+import DenseAppBar from './components/Navigation/NavBar';
 import styles from './App.module.css';
 import { fetchData } from './api';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
 import coronaImage from './images/image.png';
 
@@ -31,6 +35,9 @@ class App extends React.Component {
         return (
             <div className={styles.container}>
                 <img className={styles.image} src={coronaImage} alt="COVID-19"/>
+                <React.Fragment>
+                   <DenseAppBar>hi</DenseAppBar>
+                </React.Fragment>
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
                 <Chart data={data} country={country} />
